d7fe3af9948371ea74463f9d5045cb237b69a578
ashishchandel89 <chandel078ashish@gmail.com>
2026-02-14 12:34:57 +0530

    scale add

--- Dom2/script.js
+++ Dom2/script.js
@@ -1,6 +1,7 @@
 let btn=document.querySelector('.btn');
 let main=document.querySelector('main');
 let body=document.querySelector('body');
+let h1=document.querySelector('h1');
 main.addEventListener("mousemove",function(){
     let div=document.createElement('div');
     let left=Math.random()*100;
@@ -11,16 +12,17 @@ main.addEventListener("mousemove",function(){
     let rot=Math.floor(Math.random()*360);
     let scale=Math.floor(Math.random()*2);
 
-   div.style.width="20px";
-   div.style.height="20px";
-   div.style.borderRadius="50%"
-   div.style.border = `1px solid  rgb(128, 254, 3))`
-  div.style.boxShadow=" 30px 30px 30px rgb(128, 254, 3)"
-   div.style.position="absolute";
-   div.style.left=`${left}%`;
+    div.style.width="20px";
+    div.style.height="20px";
+    div.style.borderRadius="50%"
+    div.style.border = `1px solid  rgb(128, 254, 3))`
+    div.style.boxShadow=" 30px 30px 30px rgb(128, 254, 3)"
+    div.style.position="absolute";
+    div.style.left=`${left}%`;
     div.style.top=`${top}%`;
     div.style.rotate=`${rot}deg`;
     div.style.scale=`${scale}`;
+    h1.style.scale=`${scale}`;
 
    body.appendChild(div);
 });
