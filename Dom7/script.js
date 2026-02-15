0b377048b2a0d2474c7295b0f200cf5f0b559163
ashishchandel89 <chandel078ashish@gmail.com>
2026-02-15 17:19:46 +0530

    complete task of cursor making

--- Dom7/script.js
+++ Dom7/script.js
@@ -1,7 +1,9 @@
 let main=document.querySelector('main');
+let pointer=document.querySelector('#cursor');
 
-main.addEventListener("mousemove",function(val){
-    
+main.addEventListener("mousemove",function(dets){
+    pointer.style.left=dets.x+'px';
+    pointer.style.top=dets.y+'px';
 })
 
 
