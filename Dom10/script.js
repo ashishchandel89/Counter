98d7050df864ab37b03c45e5b495aa3bad6ecc53
ashishchandel89 <chandel078ashish@gmail.com>
2026-02-18 13:16:26 +0530

    add and remove friend with querySlector all

--- Dom10/script.js
+++ Dom10/script.js
@@ -1,1 +1,14 @@
-let All=document.querySelectorAll('#elem');
+let Allbtn=document.querySelectorAll(".btn");
+
+Allbtn.forEach(function(elem){
+        elem.addEventListener("click",function(){
+            if(elem.innerHTML=='Add'){
+                elem.innerHTML="Remove";
+                elem.style.backgroundColor="red";
+            }
+            else{
+                elem.innerHTML="Add";
+                elem.style.backgroundColor="purple";
+            }
+        })
+})
