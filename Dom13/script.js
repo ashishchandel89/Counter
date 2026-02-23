9889d01c3533f20fbdd75cb5f778acde27af6466
ashishchandel89 <chandel078ashish@gmail.com>
2026-02-23 14:34:07 +0530

    add

--- Dom13/script.js
+++ Dom13/script.js
@@ -1,11 +1,12 @@
-let para=document.querySelector('p');
+let para=document.querySelector("p");
 const char="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 const text=para.innerText;
-para.addEventListener("mouseenter" , function(){
+
+para.addEventListener('mouseenter',function(){
     setInterval(function(){
-        const str=text.split('').map((ch,index)=>{
+        const str=text.split('').map(function(ch,idx){
             return char.split('')[Math.floor(Math.random()*53)];
         }).join('');
         para.innerText=str;
-    },30)
-});
+        },30); 
+    });
