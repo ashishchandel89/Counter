83d52bb74250b330f8edfafc108198b80afb98fc
ashishchandel89 <chandel078ashish@gmail.com>
2026-02-24 21:51:31 +0530

    complete program

--- Dom13/script.js
+++ Dom13/script.js
@@ -2,11 +2,21 @@ let para=document.querySelector("p");
 const char="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 const text=para.innerText;
 
+let iteration=0;
 para.addEventListener('mouseenter',function(){
-    setInterval(function(){
+        function randomText(){
+            
         const str=text.split('').map(function(ch,idx){
+            if(iteration>idx){
+                return ch;
+            }
             return char.split('')[Math.floor(Math.random()*53)];
         }).join('');
         para.innerText=str;
-        },30); 
+        iteration+=0.25;
+    }
+    setInterval(function(){
+        randomText();
+    },9.9)
+        
     });
